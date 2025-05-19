3534bcea626f8d4def5bc80687582e706ec7f43a
Debojyoti Dutta <debojyotidutta94@gmail.com>
2025-05-19 12:11:45 +0000

    Node Class Constructor

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,9 @@
+# Time Complexity - O(1)
+# Space Complexity - O(1)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
+node = Node(10)
+print(node)
